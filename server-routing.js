fe4189e7a22c4af580abc7ed64a87351f6c4c489
Jarrod Daniels <jcdaniels@umass.edu>
2020-04-22 10:46:29 -0400

    converting typescript to js for heroku deployment

---
+++ server-routing.js
@@ -0,0 +1,137 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var http = require('http');
+var url = require('url');
+var express = require('express');
+var MyServer = /** @class */ (function () {
+    function MyServer(db) {
+        // Server stuff: use express instead of http.createServer
+        this.server = express();
+        this.port = 8080;
+        this.router = express.Router();
+        this.theDatabase = db;
+        // from https://enable-cors.org/server_expressjs.html
+        this.router.use(function (request, response, next) {
+            response.header('Content-Type', 'application/json');
+            response.header('Access-Control-Allow-Origin', '*');
+            response.header('Access-Control-Allow-Headers', '*');
+            next();
+        });
+        // Serve static pages from a particular path.
+        this.server.use(express.static(__dirname + '/html'));
+        //Handle CREATE operation
+        this.router.get('/create', this.createSightingHandler.bind(this));
+        this.router.get('/view', this.viewSightingHandler.bind(this));
+        //start
+        this.server.use('/sightingsEntry', this.router);
+    }
+    MyServer.prototype.createSightingHandler = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.createSighting(request.query.species, request.query.date, request.query.time, request.query.loc, request.query.lat, request.query.long, request.query.gender, request.query.size, request.query.amount, response)];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    MyServer.prototype.viewSightingHandler = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.viewSighting(request.query.species, response)];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    MyServer.prototype.listen = function (port) {
+        this.server.listen(port);
+    };
+    //private async createSighting(species: string, date: Date, time: number, Lat: number, Long: number, gender: string, size: number, amt : number, response): Promise<void> {
+    //	}
+    MyServer.prototype.createSighting = function (species, date, time, loc, lat, long, gender, size, amount, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        console.log("creating sighting entry for '" + species + "'");
+                        return [4 /*yield*/, this.theDatabase.put(species, [date, time, loc, lat, long, gender, size, amount])];
+                    case 1:
+                        _a.sent();
+                        response.write(JSON.stringify({ 'Species': species,
+                            'Date': date,
+                            'Time': time,
+                            'location': loc,
+                            'Latitude': lat,
+                            'Longitude': long,
+                            'Gender': gender,
+                            'Size': size,
+                            'Amount Seen': amount
+                        }));
+                        response.end();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    MyServer.prototype.viewSighting = function (species, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var value;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.theDatabase.get(species)];
+                    case 1:
+                        value = _a.sent();
+                        response.write(JSON.stringify({ 'result': 'read',
+                            'Species': species,
+                            'values': value }));
+                        response.end();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    return MyServer;
+}());
+exports.MyServer = MyServer;
